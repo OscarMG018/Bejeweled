68d3e994ed966d51135e23cd3a38722bb91997ab
OscarMG018 <108469203+OscarMG018@users.noreply.github.com>
2024-08-28 14:08:24 +0200

    Implemented swap and special Jewels

--- app/src/main/java/com/example/bejeweled/Game/Board.kt
+++ app/src/main/java/com/example/bejeweled/Game/Board.kt
@@ -1,17 +1,82 @@
 package com.example.bejeweled.Game
 import androidx.compose.ui.graphics.Color
+import kotlin.math.*
 
 enum class JewelColor {
     Red,
     Yellow,
     Green,
-    Blue
+    Blue,
+    FireryRed,
+    FireryYellow,
+    FireryGreen,
+    FireryBlue,
+    ColorCube;
+
+    fun getGroup(): ColorGroup {
+        return when (this) {
+            Red, FireryRed -> ColorGroup.Red
+            Yellow, FireryYellow -> ColorGroup.Yellow
+            Green, FireryGreen -> ColorGroup.Green
+            Blue, FireryBlue -> ColorGroup.Blue
+            ColorCube -> ColorGroup.ColorCube
+        }
+    }
+
+    fun getFireryVersion(): JewelColor {
+        return when(this) {
+            Red -> FireryRed
+            Yellow -> FireryYellow
+            Green -> FireryGreen
+            Blue -> FireryBlue
+            FireryRed -> FireryRed
+            FireryYellow -> FireryYellow
+            FireryGreen -> FireryGreen
+            FireryBlue -> FireryBlue
+            ColorCube -> ColorCube
+        }
+    }
+
+    fun isFirery(): Boolean {
+        return when(this) {
+            Red -> false
+            Yellow -> false
+            Green -> false
+            Blue -> false
+            FireryRed -> true
+            FireryYellow -> true
+            FireryGreen -> true
+            FireryBlue -> true
+            ColorCube -> false
+        }
+    }
 }
 
-data class Position(val x: Int, val y: Int)
+enum class ColorGroup {
+    Red, Yellow, Green, Blue, ColorCube
+}
 
-data class Block(val positions: List<Position>, val color: JewelColor) {
-    fun isValid() = positions.size > 3
+data class Position(val x: Int, val y: Int) {
+    fun isOrthogonallyAdjacent(other: Position): Boolean {
+        return (this.x == other.x && (this.y == other.y + 1 || this.y == other.y - 1)) ||
+                (this.y == other.y && (this.x == other.x + 1 || this.x == other.x - 1))
+    }
+}
+
+data class Block(val positions: List<Position>,val color:JewelColor) {
+    fun isValid() = positions.size > 2
+    fun center(): Position? {
+        if (positions.isEmpty()) return null
+
+        val avgX = positions.map { it.x }.average()
+        val avgY = positions.map { it.y }.average()
+
+        // Round the average coordinates to the nearest integer
+        val centerX = avgX.roundToInt()
+        val centerY = avgY.roundToInt()
+
+        return Position(centerX, centerY)
+    }
 }
 
 class Board {
@@ -19,10 +84,23 @@ class Board {
     private val array : Array<Array<JewelColor?>> = Array(9) { row ->
         arrayOfNulls<JewelColor>(9)
     }
+    var points:Int = 0
+    var moves:Int = 0
+    var comboMultiplier:Float = 1f
+    var Lastpos1:Position = Position(-1,-1)
+    var Lastpos2:Position = Position(-1,-1)
 
     // Example method to initialize some cells
     constructor() {
         initBoard()
+    }
+
+    fun Swap(pos1:Position,pos2:Position) {
+        Lastpos1 = pos1
+        Lastpos2 = pos2
+        val aux:JewelColor? = array[pos1.x][pos1.y]
+        array[pos1.x][pos1.y] = array[pos2.x][pos2.y]
+        array[pos2.x][pos2.y] = aux
     }
 
     fun setCell(row:Int,col:Int,color:JewelColor?) {
@@ -33,60 +111,77 @@ class Board {
         return array
     }
 
-    private fun detectBlocks(): Array<Block> {
-        val rows = array.size
-        val cols = array[0].size
-        val visited = Array(rows) { BooleanArray(cols) }
+    private fun detectBlocks(): List<Block> {
+        val matchingBlocks = mutableListOf<Block>()
 
-        val directions = arrayOf(
-            Position(-1, 0), // Up
-            Position(1, 0),  // Down
-            Position(0, -1), // Left
-            Position(0, 1)   // Right
-        )
-
-        fun isValidPosition(p: Position): Boolean {
-            return p.x in 0 until rows && p.y in 0 until cols
-        }
-
-        fun dfs(start: Position, color: JewelColor): List<Position> {
-            val stack = mutableListOf(start)
-            val blockPositions = mutableListOf<Position>()
-            visited[start.x][start.y] = true
-
-            while (stack.isNotEmpty()) {
-                val position = stack.removeAt(stack.size - 1)
-                blockPositions.add(position)
-
-                for (dir in directions) {
-                    val newPos = Position(position.x + dir.x, position.y + dir.y)
-                    if (isValidPosition(newPos) && !visited[newPos.x][newPos.y] && array[newPos.x][newPos.y] == color) {
-                        visited[newPos.x][newPos.y] = true
-                        stack.add(newPos)
-                    }
+        // Check horizontal lines
+        for (row in 0 until 9) {
+            var startCol = 0
+            while (startCol < 9) {
+                val color = array[row][startCol]
+                if (color == null) {
+                    startCol++
+                    continue
                 }
-            }
-            return blockPositions
-        }
 
-        val blocks = mutableListOf<Block>()
-
-        for (i in 0 until rows) {
-            for (j in 0 until cols) {
-                if (!visited[i][j]) {
-                    val color = array[i][j] ?: continue
-                    val blockPositions = dfs(Position(i, j), color)
-                    if (blockPositions.size > 3) {
-                        blocks.add(Block(blockPositions, color))
-                    }
+                var endCol = startCol
+                while (endCol < 9 && AreSameColor(array[row][endCol]!!, color)) {
+                    endCol++
                 }
+
+                if (endCol - startCol >= 3) {
+                    val positions = mutableListOf<Position>()
+                    for (col in startCol until endCol) {
+                        positions.add(Position(row, col))
+                    }
+                    matchingBlocks.add(Block(positions,color))
+                }
+
+                startCol = endCol
             }
         }
 
-        return blocks.toTypedArray()
+        // Check vertical lines
+        for (col in 0 until 9) {
+            var startRow = 0
+            while (startRow < 9) {
+                val color = array[startRow][col]
+                if (color == null) {
+                    startRow++
+                    continue
+                }
+
+                var endRow = startRow
+                while (endRow < 9 && AreSameColor(array[endRow][col]!!, color)) {
+                    endRow++
+                }
+
+                if (endRow - startRow >= 3) {
+                    val positions = mutableListOf<Position>()
+                    for (row in startRow until endRow) {
+                        positions.add(Position(row, col))
+                    }
+                    matchingBlocks.add(Block(positions,color))
+                }
+
+                startRow = endRow
+            }
+        }
+
+        return matchingBlocks.filter { it.isValid() }
     }
 
-    private fun initBoard() {
+    private fun AreSameColor(color1:JewelColor,color2:JewelColor) : Boolean {
+        return color1.getGroup() == color2.getGroup()
+    }
+
+    fun initBoard() {
+        Lastpos1 = Position(-1,-1)
+        Lastpos2 = Position(-1,-1)
+        points = 0
+        moves = 0
+        comboMultiplier = 1f
+        EamtyBoard()
         while (true) {
             val found = FillBoard()
             if (!found)
@@ -99,7 +194,7 @@ class Board {
     }
 
     private fun FillBoard(): Boolean {
-        val colors = JewelColor.values()
+        val colors: Array<JewelColor> = arrayOf(JewelColor.Red, JewelColor.Blue, JewelColor.Green, JewelColor.Yellow)
         var found = false
         for (row in 0 until 9) {
             for (col in 0 until 9) {
@@ -112,25 +207,139 @@ class Board {
         return found
     }
 
+    private fun EamtyBoard() {
+        for (row in 0 until 9) {
+            for (col in 0 until 9) {
+                array[row][col] = null
+            }
+        }
+    }
+
+    fun Explode(center: Position,multiplier:Float=1f) {
+        points += (multiplier*500*comboMultiplier).roundToInt()
+        val explosionColor = array[center.x][center.y]
+        array[center.x][center.y] = null
+        for (dx in -1..1) {
+            for (dy in -1..1) {
+                val x = center.x + dx
+                val y = center.y + dy
+                if (x in array.indices && y in array[0].indices) {
+                    if (array[x][y] != null && array[x][y]!!.isFirery()) {
+                        Explode(Position(x, y),multiplier=multiplier*1.5f)
+                    }
+                    if (array[x][y] != null && array[x][y]!!.getGroup() == ColorGroup.ColorCube) {
+                        array[x][y] = null
+                        Destroy(explosionColor!!.getGroup(),multiplier=multiplier*1.5f)
+                    }
+                    array[x][y] = null
+                }
+            }
+        }
+    }
+
+    fun Destroy(color:ColorGroup,multiplier:Float= 1f) {
+        for (x in array.indices) {
+            for (y in array[x].indices) {
+                if (array[x][y]?.getGroup() == color || color == ColorGroup.ColorCube) {
+                    points += (650*comboMultiplier*multiplier).roundToInt()
+                    if (array[x][y] != null && array[x][y]!!.isFirery()) {
+                        Explode(Position(x, y),multiplier=multiplier*1.5f)
+                    }
+                    array[x][y] = null  // Or appropriate logic to clear the jewel
+                }
+            }
+        }
+    }
+
     private fun DeleteBlock(block:Block) {
-        for (pos in block.positions) {
-            array[pos.x][pos.y] = null
+        for (positon in block.positions) {
+            array[positon.x][positon.y] = null
+        }
+    }
+
+    private fun ActivateBlock(block:Block) {
+        if (block.positions.size == 4) {
+            var specialPosition: Position = Position(-1, -1)
+            if (block.positions.contains(Lastpos1))
+                specialPosition = Lastpos1
+            else if (block.positions.contains((Lastpos2)))
+                specialPosition = Lastpos2
+            else
+                specialPosition = block.center()!!
+            val specialColor = block.color.getFireryVersion()
+            for (i in  0 until block.positions.size) {
+                if (array[block.positions[i].x][block.positions[i].y] != null && array[block.positions[i].x][block.positions[i].y]!!.isFirery())
+                    Explode(block.positions[i])
+                array[block.positions[i].x][block.positions[i].y] = null
+            }
+            array[specialPosition.x][specialPosition.y] = specialColor
+            return
+        }
+        if (block.positions.size == 5) {
+            var specialPosition: Position = Position(-1, -1)
+            if (block.positions.contains(Lastpos1))
+                specialPosition = Lastpos1
+            else if (block.positions.contains((Lastpos2)))
+                specialPosition = Lastpos2
+            else
+                specialPosition = block.center()!!
+            val specialColor = JewelColor.ColorCube
+            for (i in  0 until block.positions.size) {
+                if (array[block.positions[i].x][block.positions[i].y] != null && array[block.positions[i].x][block.positions[i].y]!!.isFirery())
+                    Explode(block.positions[i])
+                array[block.positions[i].x][block.positions[i].y] = null
+            }
+            array[specialPosition.x][specialPosition.y] = specialColor
+            return
+        }
+        for (i in  0 until block.positions.size) {
+            if (array[block.positions[i].x][block.positions[i].y] != null && array[block.positions[i].x][block.positions[i].y]!!.isFirery())
+                Explode(block.positions[i])
+            array[block.positions[i].x][block.positions[i].y] = null
         }
     }
 
     fun CheckMove() {
+        if (array[Lastpos1.x][Lastpos1.y] == JewelColor.ColorCube) {
+            array[Lastpos1.x][Lastpos1.y] = null
+            Destroy(array[Lastpos2.x][Lastpos2.y]!!.getGroup())
+        }
+        else if (array[Lastpos2.x][Lastpos2.y] == JewelColor.ColorCube) {
+            array[Lastpos2.x][Lastpos2.y] = null
+            Destroy(array[Lastpos1.x][Lastpos1.y]!!.getGroup())
+        }
         MakeBlocksFall()
         FillBoard()
         while (true) {
             val detectedBlocks = detectBlocks()
-            if (detectedBlocks.isEmpty())
+            if (detectedBlocks.isEmpty()) {
+                println("exit")
+                comboMultiplier = 1f
                 return
-            for (block in detectedBlocks) {
-                DeleteBlock(block)
             }
+            for (block in detectedBlocks) {
+                BlockPoints(block)
+                ActivateBlock(block)
+                comboMultiplier += 0.1f
+            }
+            println("deleting again")
             MakeBlocksFall()
             FillBoard()
         }
+    }
+
+    private fun BlockPoints(block: Block) {
+        if (block.positions.size == 3) {
+            points += (60*comboMultiplier).roundToInt()
+        }
+        else if (block.positions.size == 4) {
+            points += (120*comboMultiplier).roundToInt()
+        }
+        else {
+            points += (200*comboMultiplier).roundToInt()
+        }
+
+        println(points)
     }
 
     private fun MakeBlocksFall() {
--- app/src/main/java/com/example/bejeweled/MainActivity.kt
+++ app/src/main/java/com/example/bejeweled/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.bejeweled
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.view.inputmethod.InputBinding
 import androidx.activity.ComponentActivity
@@ -26,86 +27,149 @@ import androidx.compose.runtime.*
 import kotlinx.coroutines.flow.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.activity.viewModels
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.ui.text.style.TextAlign
+import com.example.bejeweled.Game.Position
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+import java.io.BufferedReader
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.InputStreamReader
 
 class GameViewModel : ViewModel() {
     // Create an instance of the Board
     private val board: Board = Board()
+    var pos1: Position = Position(-1,-1)
+    var pos2: Position = Position(-1,-1)
 
-    private val _boardLiveData  = MutableLiveData<Array<Array<JewelColor?>>>()
+    private val _boardLiveData = MutableLiveData<Array<Array<JewelColor?>>>()
+    private val _pointLiveData = MutableLiveData<Int>()
+    private val _movesLiveData = MutableLiveData<Int>()
+    private val _highScoreLiveData = MutableLiveData<Int>()
     val boardLiveData: LiveData<Array<Array<JewelColor?>>>
         get() = _boardLiveData
+    val pointsLiveData: LiveData<Int>
+        get() = _pointLiveData
+    val movesLiveData: LiveData<Int>
+        get() = _movesLiveData
+    val highScoreLiveData: LiveData<Int>
+        get() = _highScoreLiveData
 
     init {
         _boardLiveData.value = board.getState()
+        _pointLiveData.value = board.points
+        _movesLiveData.value = board.moves
+        _highScoreLiveData.value = 0
     }
 
-    fun setCell(row: Int, col: Int, color: JewelColor?) {
-        board.setCell(row,col,color)
-        updateBoardLiveData()
+    fun saveHighScore(context: Context) {
+        val currentPoints = pointsLiveData.value ?: 0
+        val previousRecord = loadRecord(context)?.toIntOrNull() ?: 0
+
+        if (currentPoints > previousRecord) {
+            saveRecord(context, currentPoints.toString())
+        }
     }
 
-    fun CheckMove() {
-        board.CheckMove()
-        updateBoardLiveData()
+    fun Reset() {
+        board.initBoard()
+        pos1 = Position(-1,-1)
+        pos2 = Position(-1,-1)
+        updateLiveData()
     }
 
-    private fun updateBoardLiveData() {
+    fun HandleClick(pos: Position) {
+        if (pos1.x == -1) {
+            pos1 = pos
+            updateLiveData()
+            println("Pos1 Set")
+        }
+        else {
+            if (pos1.isOrthogonallyAdjacent(pos)) {
+                pos2 = pos
+                println("Pos2 Set")
+                board.Swap(pos1,pos2)
+                board.CheckMove()
+                board.moves++
+            }
+            pos1 = Position(-1,-1)
+            println("Pos1 Reset")
+            pos2 = Position(-1,-1)
+            println("Pos2 Reset")
+
+            updateLiveData()
+        }
+    }
+
+    private fun updateLiveData() {
         _boardLiveData.value = board.getState().copyOf()
+        _pointLiveData.value = board.points
+        _movesLiveData.value = board.moves
+        if ((_pointLiveData.value ?: 0) > (_highScoreLiveData.value ?: 0))
+            _highScoreLiveData.value = _pointLiveData.value
+    }
+
+    fun SetHighScore(value:Int) {
+        _highScoreLiveData.value = value
     }
 }
+
 class MainActivity : ComponentActivity() {
 
     private val boardViewModel: GameViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //deleteRecord(this)
+        boardViewModel.SetHighScore(loadRecord(this)?.toIntOrNull() ?: 0)
         setContent {
             BejeweledTheme {
-                DisplayBoard(boardViewModel)
-            }
-        }
-    }
-}
+                var showDialog by remember { mutableStateOf(false) }
 
-@Composable
-fun DisplayBoard(viewModel: GameViewModel) {
-
-    val boardState by viewModel.boardLiveData.observeAsState(emptyArray())
-
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding),
-            contentAlignment = Alignment.Center
-        ) {
-            Column(
-                modifier = Modifier
-                    .wrapContentSize()
-                    .background(Color.LightGray)
-            ) {
-                for (row in 0 until 9) {
-                    Row(
-                        modifier = Modifier.wrapContentSize()
+                // Check if the player has run out of moves
+                val moves by boardViewModel.movesLiveData.observeAsState(0)
+                if (moves >= 50) {
+                    showDialog = true
+                    boardViewModel.saveHighScore(this)
+                }
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Top,
+                        horizontalAlignment = Alignment.CenterHorizontally // Center the content horizontally
                     ) {
-                        for (col in 0 until 9) {
-                            val color = boardState[row][col]
-                            Box(
-                                modifier = Modifier
-                                    .size(40.dp)
-                                    .background(getColorForDisplay(color))
-                                    .border(1.dp, Color.Gray)
-                                    .clickable {
-                                        println("click")
-                                        if (boardState[row][col] == null) {
-                                            viewModel.CheckMove()
-                                            return@clickable
-                                        }
-                                        viewModel.setCell(row, col, null)
-                                        println(boardState[row][col])
-                                    }
-                            )
+                        Box(
+                            modifier = Modifier
+                                .padding(top = 60.dp),
+                        ) {
+                            PointsText(boardViewModel)
                         }
+                        Box(
+                            modifier = Modifier
+                                .padding(top = 0.dp),
+                        ) {
+                            MovesText(boardViewModel)
+                        }
+                        Box(
+                            modifier = Modifier
+                                .padding(top = 0.dp),
+                        ) {
+                            HighScore(boardViewModel)
+                        }
+                    }
+                    DisplayBoard(boardViewModel, innerPadding)
+
+                    // Show the game over dialog when the player runs out of moves
+                    if (showDialog) {
+                        GameOverDialog(
+                            viewModel = boardViewModel,
+                            onDismiss = { showDialog = false },
+                            onExit = { finish() } // Exit the activity when the "Exit" button is clicked
+                        )
                     }
                 }
             }
@@ -113,28 +177,152 @@ fun DisplayBoard(viewModel: GameViewModel) {
     }
 }
 
-fun getColorForDisplay(color: JewelColor?): androidx.compose.ui.graphics.Color {
-    return when (color) {
-        JewelColor.Red -> androidx.compose.ui.graphics.Color.Red
-        JewelColor.Yellow -> androidx.compose.ui.graphics.Color.Yellow
-        JewelColor.Green -> androidx.compose.ui.graphics.Color.Green
-        JewelColor.Blue -> androidx.compose.ui.graphics.Color.Blue
-        else -> androidx.compose.ui.graphics.Color.Black
-    }
-}
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun GameOverDialog(viewModel: GameViewModel, onDismiss: () -> Unit, onExit: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = { /* Do nothing, the dialog is blocking */ },
+        title = {
+            Text(text = "Game Over")
+        },
+        text = {
+            Text("You've run out of moves! Do you want to reset the game?")
+        },
+        confirmButton = {
+            Button(onClick = {
+                viewModel.Reset() // Reset the board when the button is clicked
+                onDismiss() // Dismiss the dialog
+            }) {
+                Text("Reset Game")
+            }
+        },
+        dismissButton = {
+            Button(onClick = { onExit() }) {
+                Text("Exit")
+            }
+        }
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    BejeweledTheme {
-        Greeting("Android")
+fun MovesText(viewModel: GameViewModel) {
+    val moves by viewModel.movesLiveData.observeAsState(0)
+
+    Text(text = "${50-moves} Moves Left", fontSize = 25.sp)
+}
+
+@Composable
+fun PointsText(viewModel: GameViewModel) {
+    val points by viewModel.pointsLiveData.observeAsState(0)
+
+    Text(text = "$points Points",fontSize = 25.sp)
+}
+
+@Composable
+fun HighScore(viewModel: GameViewModel) {
+    val highScore by viewModel.highScoreLiveData.observeAsState(0)
+
+    Text(text = "Your Record is $highScore Points",fontSize = 25.sp)
+}
+
+@Composable
+fun DisplayBoard(viewModel: GameViewModel,pad : PaddingValues) {
+
+    val boardState by viewModel.boardLiveData.observeAsState(emptyArray())
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .wrapContentSize()
+                .background(Color.LightGray)
+        ) {
+            for (row in 0 until 9) {
+                Row(
+                    modifier = Modifier.wrapContentSize()
+                ) {
+                    for (col in 0 until 9) {
+                        val color = boardState[row][col]
+                        val colors = getColorsForDisplay(color)
+                        val position = Position(row,col)
+                        val borderColor = if (position == viewModel.pos1) Color.White else Color.Gray
+                        val borderWidth = if (position == viewModel.pos1) 4 else 1
+                        Box(
+                            modifier = Modifier
+                                .size(40.dp)
+                                .background(
+                                    brush =
+                                    Brush.linearGradient(
+                                        colors = colors
+                                    )
+                                )
+                                .border(borderWidth.dp, borderColor)
+                                .clickable {
+                                    viewModel.HandleClick(Position(row, col))
+                                }
+                        )
+                    }
+                }
+            }
+        }
     }
 }
+
+fun getColorsForDisplay(color: JewelColor?): List<Color> {
+    return when (color) {
+        JewelColor.Red -> listOf(Color.Red, Color.Red)
+        JewelColor.Yellow -> listOf(Color.Yellow,Color.Yellow)
+        JewelColor.Green -> listOf(Color.Green,Color.Green)
+        JewelColor.Blue -> listOf(Color.Blue,Color.Blue)
+        JewelColor.FireryRed -> listOf(Color.hsl(300f,0.5f,0.45f),Color.Red,Color.hsl(300f,0.5f,0.45f))
+        JewelColor.FireryYellow -> listOf(Color.hsl(20f,1f,0.5f),Color.Yellow,Color.hsl(20f,1f,0.5f))
+        JewelColor.FireryGreen -> listOf(Color.hsl(95f,1f,0.3f),Color.Green,Color.hsl(95f,1f,0.3f))
+        JewelColor.FireryBlue -> listOf(Color.Cyan,Color.Blue,Color.Cyan)
+        JewelColor.ColorCube -> listOf(Color.Black,Color.White)
+        else ->listOf(Color.Black,Color.Black)
+    }
+}
+
+fun saveRecord(context: Context, record: String) {
+    val fileName = "game_record.txt"
+    val fileOutputStream: FileOutputStream
+
+    try {
+        fileOutputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE)
+        fileOutputStream.write(record.toByteArray())
+        fileOutputStream.close()
+        println("Record saved successfully")
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+
+fun loadRecord(context: Context): String? {
+    val fileName = "game_record.txt"
+    var record: String? = null
+
+    try {
+        val fileInputStream: FileInputStream = context.openFileInput(fileName)
+        val inputStreamReader = InputStreamReader(fileInputStream)
+        val bufferedReader = BufferedReader(inputStreamReader)
+
+        record = bufferedReader.readLine()
+
+        bufferedReader.close()
+        inputStreamReader.close()
+        fileInputStream.close()
+        println("Record loaded successfully")
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+
+    return record
+}
+
+fun deleteRecord(context: Context): Boolean {
+    val fileName = "game_record.txt"
+    return context.deleteFile(fileName)
+}
+
